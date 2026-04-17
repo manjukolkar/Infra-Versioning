b907a36e5d326c0418d6a69eb64b36caf4ad7faa
manjukolkar <manjunathkolkar2019@gmail.com>
2026-04-17 23:22:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,5 +13,5 @@ variable "ec2_instance_type" {
 variable "bucket_name" {
   description = "S3 bucket name"
   type        = string
-  default     = "infra-versioning-state-bucket-manjunath"
+  default     = "infra-demo-app-bucket-manjunath"
 }
