106d215b6abd4a2c69edce2bdefa6c397953a3a9
manjukolkar <manjunathkolkar2019@gmail.com>
2026-04-18 00:16:14 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,44 +1,38 @@
 #!/bin/bash
+set -e
 
-yum update -y
+apt update -y
 
 # Install Git
-yum install git -y
+apt install git -y
 
 # Install Docker
-amazon-linux-extras install docker -y
+apt install docker.io -y
 systemctl start docker
 systemctl enable docker
-usermod -aG docker ec2-user
+usermod -aG docker ubuntu
 
-# Install Java (required for Jenkins)
-yum install java-21 -y
+# Install Java
+apt update
+apt install fontconfig openjdk-21-jre
 
 # Install Jenkins
-wget -O /etc/yum.repos.d/jenkins.repo \
-https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install jenkins -y
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+apt update -y
+apt install jenkins -y
 systemctl start jenkins
 systemctl enable jenkins
-
-# Install Snap (for MicroK8s)
-yum install snapd -y
-systemctl enable --now snapd.socket
-ln -s /var/lib/snapd/snap /snap
-
-sleep 60
 
 # Install MicroK8s
 snap install microk8s --classic
 
-usermod -aG docker jenkins
-usermod -aG microk8s jenkins
+usermod -a -G microk8s ubuntu
+usermod -a -G docker ubuntu
 
-# Wait for MicroK8s
 microk8s status --wait-ready
-
-# Enable addons
 microk8s enable dns storage
-
-echo "Setup Completed"
